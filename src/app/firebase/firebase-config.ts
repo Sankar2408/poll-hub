0b342988aa72b24b2643c048a15c5523c446e156
Sankar2408 <sankarsn2004@gmail.com>
2025-05-06 12:19:19 +0530

    first commit

---
+++ src/app/firebase/firebase-config.ts
@@ -0,0 +1,11 @@
+// src/app/firebase/firebase-config.ts
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyDniPwriNufrSVspzkWNNjLnpTN1-0apvk",
+    authDomain: "poll-hub-5d146.firebaseapp.com",
+    projectId: "poll-hub-5d146",
+    storageBucket: "poll-hub-5d146.appspot.com",
+    messagingSenderId: "671377416571",
+    appId: "1:671377416571:web:a8c1d0599bc5aa6b2addde"
+  };
+  
